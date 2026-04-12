715ea459db3384eea84195a925706e4e02dd2f0e
AadityaRJ01 <1ms23cs002@msrit.edu>
2026-04-12 22:51:10 +0530

    Codebase Setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv"
-
+import { ENV } from "./lib/env.js";
 dotenv.config();
 const app=express();
 
@@ -8,4 +8,4 @@ app.get("/",(req,res)=>{
     res.status(200).json({msg:"Success from api"})
 })
 
-app.listen(3000,()=> console.log("Sever is running on port 3000"));
+app.listen(ENV.PORT,()=> console.log("Sever is running on port:",ENV.PORT));
