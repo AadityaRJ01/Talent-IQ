d00825e95d130e1b8ee11ea3f5ff7b23f29e7473
AadityaRJ01 <1ms23cs002@msrit.edu>
2026-04-20 19:15:32 +0530

    Landing Page Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,7 +20,8 @@ function App() {
         <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />} />
         <Route path="/dashboard" element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />} />
 
-        {/* <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
+        <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
+        {/*
         <Route path="/problem/:id" element={isSignedIn ? <ProblemPage /> : <Navigate to={"/"} />} />
         <Route path="/session/:id" element={isSignedIn ? <SessionPage /> : <Navigate to={"/"} />} /> */}
       </Routes>
